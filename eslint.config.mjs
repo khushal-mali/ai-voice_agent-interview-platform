c0d6f670c1be183e12ee443a9dc16ec2e2e45625
Khushal Mali <khushalmali8448@gmail.com>
2025-04-09 17:10:52 +0530

    update

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -10,7 +10,11 @@ const compat = new FlatCompat({
 });
 
 const eslintConfig = [
-  ...compat.extends("next/core-web-vitals", "next/typescript"),
+  ...compat.extends("next/core-web-vitals", "next/typescript", {
+    rules: {
+      "@typescript-eslint/no-implicit-any": "warn", // Enforces no implicit any in TypeScript files
+    },
+  }),
 ];
 
 export default eslintConfig;
