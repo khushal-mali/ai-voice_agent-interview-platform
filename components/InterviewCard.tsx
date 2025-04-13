63d819e098b282eb11ece643b2d2e56e4f28f60c
Khushal Mali <khushalmali8448@gmail.com>
2025-04-13 17:16:43 +0530

    feedback score

--- components/InterviewCard.tsx
+++ components/InterviewCard.tsx
@@ -4,8 +4,9 @@ import Image from "next/image";
 import Link from "next/link";
 import DisplayTechItems from "./DisplayTechItems";
 import { Button } from "./ui/button";
+import { getFeedbackByInterviewId } from "@/lib/actions/general.action";
 
-const InterviewCard = ({
+const InterviewCard = async ({
   id,
   userId,
   role,
@@ -13,7 +14,8 @@ const InterviewCard = ({
   techstack,
   createdAt,
 }: InterviewCardProps) => {
-  const feedback = null as Feedback | null;
+  const feedback =
+    userId && id ? await getFeedbackByInterviewId({ interviewId: id, userId }) : null;
   const normalizedType = /mix/gi.test(type) ? "Mixed" : type;
   const formattedDate = dayjs(feedback?.createdAt || createdAt || Date.now()).format(
     "MMM D, YYYY"
