fb026ba2bc821fb5217bc3a82a48645f1250f816
Khushal Mali <khushalmali8448@gmail.com>
2025-04-13 17:15:30 +0530

    interview and feedback actions

--- lib/actions/general.action.ts
+++ lib/actions/general.action.ts
@@ -1,38 +1,133 @@
+"use server";
+
+import { feedbackSchema } from "@/constants";
 import { db } from "@/firebase/admin";
+import { google } from "@ai-sdk/google";
+import { generateObject } from "ai";
 
-export async function getInterviewsByUserId(userId: string): Promise<Interview[] | null> {
-  const interviews = await db
-    .collection("interviews")
-    .where("userId", "==", userId)
-    .orderBy("createdAt", "desc")
-    .get();
+export async function getInterviewsByUserId(
+  userId: string
+): Promise<Interview[] | null | undefined> {
+  try {
+    const interviews = await db
+      .collection("interviews")
+      .where("userId", "==", userId)
+      .orderBy("createdAt", "desc")
+      .get();
 
-  return interviews.docs.map((doc) => ({
-    id: doc.id,
-    ...doc.data(),
-  })) as Interview[];
+    return interviews.docs.map((doc) => ({
+      id: doc.id,
+      ...doc.data(),
+    })) as Interview[];
+  } catch (error) {
+    console.log(`[fileName: 'general.action', Line Number: '23']`, error);
+  }
 }
 
 export async function getLatestInterviews(
   params: GetLatestInterviewsParams
-): Promise<Interview[] | null> {
-  const { userId, limit = 20 } = params;
-  const interviews = await db
-    .collection("interviews")
-    .orderBy("createdAt", "desc")
-    .where("finalized", "==", true)
-    .where("userId", "!=", userId)
-    .limit(limit)
-    .get();
+): Promise<Interview[] | null | undefined> {
+  try {
+    const { userId, limit = 20 } = params;
+    const interviews = await db
+      .collection("interviews")
+      .orderBy("createdAt", "desc")
+      .where("finalized", "==", true)
+      .where("userId", "!=", userId)
+      .limit(limit)
+      .get();
 
-  return interviews.docs.map((doc) => ({
-    id: doc.id,
-    ...doc.data(),
-  })) as Interview[];
+    return interviews.docs.map((doc) => ({
+      id: doc.id,
+      ...doc.data(),
+    })) as Interview[];
+  } catch (error) {
+    console.log(`[fileName: 'general.action', Line Number: '45']`, error);
+  }
 }
 
 export async function getInterviewById(id: string): Promise<Interview | null> {
   const interview = await db.collection("interviews").doc(id).get();
 
   return interview.data() as Interview | null;
+}
+
+export async function createFeedback(params: CreateFeedbackParams) {
+  const { interviewId, transcript, userId } = params;
+
+  try {
+    const formattedTranscript = transcript
+      .map(
+        (sentence: { role: string; content: string }) =>
+          `- ${sentence.role}: ${sentence.content}/n`
+      )
+      .join("");
+
+    const {
+      object: {
+        areasForImprovement,
+        categoryScores,
+        finalAssessment,
+        strengths,
+        totalScore,
+      },
+    } = await generateObject({
+      model: google("gemini-2.0-flash-001", { structuredOutputs: false }),
+      schema: feedbackSchema,
+      system:
+        "You are a professional interviewer analyzing a mock interview. Your task is to evaluate the candidate based on structured categories",
+      prompt: `
+      You are an AI interviewer analyzing a mock interview. Your task is to evaluate the candidate based on structured categories. Be thorough and detailed in your analysis. Don't be lenient with the candidate. If there are mistakes or areas for improvement, point them out.
+      Transcript:
+      ${formattedTranscript}
+
+      Please score the candidate from 0 to 100 in the following areas. Do not add categories other than the ones provided:
+      - **Communication Skills**: Clarity, articulation, structured responses.
+      - **Technical Knowledge**: Understanding of key concepts for the role.
+      - **Problem-Solving**: Ability to analyze problems and propose solutions.
+      - **Cultural & Role Fit**: Alignment with company values and job role.
+      - **Confidence & Clarity**: Confidence in responses, engagement, and clarity.
+      `,
+    });
+
+    const feedback = await db.collection("feedback").add({
+      interviewId,
+      userId,
+      totalScore,
+      categoryScores,
+      strengths,
+      areasForImprovement,
+      finalAssessment,
+      createdAt: new Date().toISOString(),
+    });
+
+    return { success: true, feedbackId: feedback.id };
+  } catch (error) {
+    console.log(`[fileName: 'general.action', Line Number: '106']`, error);
+    return { success: false };
+  }
+}
+
+export async function getFeedbackByInterviewId(
+  params: GetFeedbackByInterviewIdParams
+): Promise<Feedback | null> {
+  try {
+    const { interviewId, userId } = params;
+    const feedback = await db
+      .collection("feedback")
+      .where("interviewId", "==", interviewId)
+      .where("userId", "==", userId)
+      .limit(1)
+      .get();
+    if (feedback.empty) return null;
+    const feedbackDoc = feedback.docs[0];
+
+    return {
+      id: feedbackDoc.id,
+      ...feedbackDoc.data(),
+    } as Feedback;
+  } catch (error) {
+    console.log(`[fileName: 'general.action', Line Number: '130']`, error);
+    return {} as Feedback;
+  }
 }
